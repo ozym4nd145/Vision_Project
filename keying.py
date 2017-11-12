70397f7ca801384090fb35b9d53a0b82811358eb
Suyash Agrawal <ozym4nd145@outlook.com>
2017-11-12 18:22:39 +0530

    Added demo code

---
+++ keying.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+
+def segment_ycrcb(orig, params, tola, tolb):
+    ycrcb_im = cv2.cvtColor(orig, cv2.COLOR_BGR2YCrCb)
+    Cb_key, Cr_key = params
+    blue = ycrcb_im[:, :, 2]
+    red = ycrcb_im[:, :, 1]
+
+    diffbsq = (blue - Cb_key)**2
+    diffrsq = (red - Cr_key)**2
+    dist = np.sqrt(diffbsq + diffrsq).astype(np.float32)
+
+    mask = ((dist - tola) / (tolb - tola)).astype(np.float32)
+    mask[dist < tola] = 0.0
+    mask[dist > tolb] = 1.0
+    return mask
+
+
+def get_region(img):
+    r = cv2.selectROI("Select window of background", img=img,
+                      fromCenter=False, showCrossair=False)
+    cv2.destroyAllWindows()
+    return r
+
+
+def get_params_ycrcb(img, region):
+    ycrcb_img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb).astype(np.float32)
+    cv2.destroyAllWindows()
+    r = [int(x) for x in region]
+    region = ycrcb_img[int(region[1]):int(
+        region[1] + region[3]), int(region[0]):int(region[0] + region[2])]
+    y_mean, Cr_mean, Cb_mean = np.mean(region, axis=(0, 1))
+    y_std, Cr_std, Cb_std = np.std(region, axis=(0, 1))
+    return [Cb_mean, Cr_mean]
+
+
+def get_params_hls(img, region):
+    hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS).astype(np.float32)
+    r = [int(x) for x in region]
+    region = hls_img[int(region[1]):int(region[1] + region[3]),
+                     int(region[0]):int(region[0] + region[2])]
+    h_mean, l_mean, s_mean = np.mean(region, axis=(0, 1))
+    h_std, l_std, s_std = np.std(region, axis=(0, 1))
+    return [h_mean, h_std, l_mean, l_std, s_mean, s_std]
+
+
+def brighten(img, alpha, beta, gamma):
+    cor_img = img.astype(np.float32)
+    bright_img = alpha * cor_img + beta
+    gam_cor = np.power(bright_img / 255, gamma) * 255
+    bright_img = gam_cor.clip(0, 255).astype(np.uint8)
+    return bright_img
+
+
+def mod_mask(mask, low, high):
+    mask = mask.copy()
+    mask[mask > high] = 1.0
+    mask[mask < low] = 0.0
+    return mask
+
+
+# def process(img, bg, param_ycrcb, param_hls, tola, tolb, low_thresh, high_thresh, alpha, beta, gamma, sz, space, erode_sz):
+def get_mask(img,param_ycrcb, tola=16, tolb=50, low_thresh=0.05, high_thresh=0.25, sz=5, space=200):
+    ##ENSURE THAT param_ycrcb and param_hls correspond to brigthened img
+
+    #     brimg = brighten(img,alpha,beta,gamma)
+    brimg = img
+    brimg = cv2.bilateralFilter(brimg, sz, space, space)
+
+    mask = segment_ycrcb(brimg, param_ycrcb, tola, tolb)
+    mask = mod_mask(mask, low_thresh, high_thresh)
+
+#     kernel = np.ones((erode_sz,erode_sz),np.uint8)
+#     mask = cv2.erode(mask,kernel,iterations = 1)
+    return mask
+    # mask = np.expand_dims(mask, -1)
+
+    # new_img = (mask * img + (1 - mask) * bg).astype(np.uint8)
+    # return (mask, new_img)
+
+def get_bgra(img,mask):
+    assert (len(mask.shape) == 2)  # (x,y)
+    assert (len(img.shape) == 3)  # (x,y,3)
+    alpha = (mask * 255).astype(np.uint8)
+    alpha = np.expand_dims(alpha,-1) ##(x,y,1)
+    return np.concatenate((img,alpha),axis=2) ##(x,y,4)
+def write_alpha_img(img, mask, path):
+    r_channel, g_channel, b_channel = cv2.split(img)
+    alpha = (mask * 255).astype(np.uint8)
+    img_RGBA = cv2.merge((r_channel, g_channel, b_channel, alpha))
+    cv2.imwrite(path, img_RGBA)
+    return img_RGBA
---
+++ projection.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect
+
+
+def get_corners_mask(mask):
+    if len(mask.shape) > 2:
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    corners = cv2.goodFeaturesToTrack(mask, 4, 0.01, 10)
+    corners = np.int0(corners)
+    corners = np.squeeze(corners)
+    ## returns in order of (topl,bottoml,topr,bottomr)
+    return order_points(corners)
+
+def get_corners_img(img):
+    ## in order of (topl,topr,bottomr,bottoml) in image coordinates
+    return np.array([[0, 0], [(img.shape[1] - 1), 0],
+                     [(img.shape[1] - 1), (img.shape[0] - 1)], [0, (img.shape[0] - 1)]], dtype=np.float32)
+
+def get_transform(imgcorners,maskcorners):
+    return cv2.getPerspectiveTransform(imgcorners, maskcorners)
+
+def project_to_mask(img,mask):
+    mask_corner = get_corners_mask(mask)
+    img_corner = get_corners_img(img)
+    transform = get_transform(img_corner,mask_corner)
+    new_img = cv2.warpPerspective(
+        img, transform, (mask.shape[1], mask.shape[0]))
+    return new_img
